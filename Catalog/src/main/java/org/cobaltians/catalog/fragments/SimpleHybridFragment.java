bea027f5d8356c1d48a72a26341542382be80066
Vincent Rifa <vincent.rifa.kristal@gmail.com>
2019-04-16 15:35:42 +0200

    added plugin geolocpicker

--- Catalog/src/main/java/org/cobaltians/catalog/fragments/SimpleHybridFragment.java
+++ Catalog/src/main/java/org/cobaltians/catalog/fragments/SimpleHybridFragment.java
@@ -1,5 +1,11 @@
 package org.cobaltians.catalog.fragments;
 
+import android.content.Intent;
+
+import org.cobaltians.cobalt.plugin.CobaltPluginWebContainer;
+
+import io.kristal.geolocpicker.GeolocPicker;
+
 public class SimpleHybridFragment extends AbstractFragment {
 
 	@Override
@@ -7,4 +13,13 @@ public class SimpleHybridFragment extends AbstractFragment {
 
 	@Override
 	protected void onInfiniteScrollRefreshed() { }
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		// Forward result to GeolocPicker
+		GeolocPicker geolocPicker = (GeolocPicker)GeolocPicker.getInstance(new CobaltPluginWebContainer(getActivity(),this));
+		geolocPicker.onActivityResult(requestCode, resultCode, data);
+		super.onActivityResult(requestCode, resultCode, data);
+	}
 }
