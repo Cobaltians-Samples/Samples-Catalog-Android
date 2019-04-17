5b0e1f68654c7e2adea46a4a6a74b8eadc8d665d
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-17 15:01:36 +0200

    Merge branch 'pubsub-withoutwww' into cobalt1.0

--- Catalog/src/main/java/org/cobaltians/catalog/fragments/CallbacksFragment.java
+++ Catalog/src/main/java/org/cobaltians/catalog/fragments/CallbacksFragment.java
@@ -1,166 +1,266 @@
 package org.cobaltians.catalog.fragments;
 
 import android.app.AlertDialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
+import org.cobaltians.cobalt.Cobalt;
+import org.cobaltians.cobalt.pubsub.PubSub;
+import org.cobaltians.cobalt.pubsub.PubSubInterface;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class CallbacksFragment extends AbstractFragment {
-
-	protected static String JSAddValues = "addValues";
-    protected static String JSValuesCallback = "valuesCallback";
-    protected static String JSEcho = "echo";
-    protected static String JSEchoCallback = "echoCallback";
-    protected static String kResult = "result";
-    protected static String kValues = "values";
-    protected static String kValue = "value";
-    protected static String kIndex = "index";
+public class CallbacksFragment extends AbstractFragment implements PubSubInterface
+{
+ 
+	private static final String JSDoSomeMathsFromNative = "doSomeMathsFromNative";
+    private static final String JSDoSomeMathsFromNativeResponse = "doSomeMathsFromNativeResponse";
+    private static final String JSDoSomeMathsFromWeb = "doSomeMathsFromWeb";
+    private static final String JSAutoTestsFromNative = "autoTestsFromNative";
+    private static final String JSAutoTestsFromNativeCallback = "autoTestsFromNativeCallback";
+    private static final String JSAutoTestsFromWeb = "autoTestsFromWeb";
+    private static final String kJSValues = "values";
+    private static final String kJSResult = "result";
+    private static final String kJSIndex = "index";
 
 	private Button btnDoSomeMath, btnTestAuto;
 
-    private ArrayList<Object> mArrayTest;
-    private boolean testFailed = false;
-
-	@Override
-	protected int getLayoutToInflate() {
+    private ArrayList<String> mArrayTest;
+    private boolean testFailed;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    
+        mArrayTest = new ArrayList<>(6);
+        mArrayTest.add("quotes : it's working \"great\"");
+        mArrayTest.add("+ is nice.");
+        mArrayTest.add("url &eactue;é&12;\n3#23:%20'\\u0020hop");
+        mArrayTest.add("'{ obj_representation : \"test\"}'");
+        mArrayTest.add("emoji \ue415 \\ue415 u{1f604}");
+        mArrayTest.add("http://42.com?key=xbE%2B49MCgE7/WTKqnvwQ3f4zYmg%3D");
+    }
+    
+    @Override
+	protected int getLayoutToInflate()
+    {
 		return org.cobaltians.catalog.R.layout.callbacks_fragment;
 	}
 
 	@Override
-	protected void setUpViews(View rootView) {
+	protected void setUpViews(View rootView)
+    {
 		super.setUpViews(rootView);
-        btnDoSomeMath = (Button) rootView.findViewById(org.cobaltians.catalog.R.id.btnDoSomeMaths);
-        btnTestAuto = (Button) rootView.findViewById(org.cobaltians.catalog.R.id.btnTestAuto);
-		}
+		
+        btnDoSomeMath = rootView.findViewById(org.cobaltians.catalog.R.id.btnDoSomeMaths);
+        btnTestAuto = rootView.findViewById(org.cobaltians.catalog.R.id.btnTestAuto);
+	}
 
 	@Override
-	protected void setUpListeners() {
-        btnDoSomeMath.setOnClickListener(new OnClickListener() {
+	protected void setUpListeners()
+    {
+        btnDoSomeMath.setOnClickListener(new OnClickListener()
+        {
 			@Override
-			public void onClick(View arg0) {
-				JSONObject data = new JSONObject();
-				try {
+			public void onClick(View arg0)
+            {
+				try
+                {
 					ArrayList<Integer> values = new ArrayList<>();
 					values.add(1);
 					values.add(3);
-					data.put(kValues, new JSONArray(values));
-					sendEvent(JSAddValues, data, JSValuesCallback);
-				} catch (JSONException e) {
+                    
+                    JSONObject message = new JSONObject();
+                    message.put(kJSValues, new JSONArray(values));
+                    message.put(Cobalt.kJSCallback, JSDoSomeMathsFromNativeResponse);
+                    
+                    PubSub.getInstance().publishMessage(message, JSDoSomeMathsFromNative);
+				}
+				catch (JSONException e)
+                {
 					e.printStackTrace();
 				}
 			}
 		});
 
-        btnTestAuto.setOnClickListener(new OnClickListener() {
+        btnTestAuto.setOnClickListener(new OnClickListener()
+        {
             @Override
-            public void onClick(View arg0) {
-                mArrayTest = new ArrayList<>();
-                mArrayTest.add(0, "quotes : it's working \"great\"");
-                mArrayTest.add(1, "+ is nice.");
-                mArrayTest.add(2, "url &eactue;é&12;\n3#23:%20'\\u0020hop");
-                mArrayTest.add(3, "'{ obj_representation : \"test\"}'");
-                mArrayTest.add(4, "emoji \ue415 \\ue415 u{1f604}");
-                mArrayTest.add(5, "http://42.com?key=xbE%2B49MCgE7/WTKqnvwQ3f4zYmg%3D");
-
+            public void onClick(View arg0)
+            {
+                testFailed = false;
                 launchTest(0);
             }
         });
 	}
-
-	@Override
-	protected boolean onUnhandledEvent(String name, JSONObject data, String callback) {
-        if (name.equals(JSAddValues)) {
-            try {
-                JSONArray values = data.getJSONArray(kValues);
-                int val1 = values.getInt(0);
-                int val2 = values.getInt(1);
-                JSONObject result = new JSONObject();
-                result.put(kResult, val1+val2);
-                sendCallback(callback, result);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return true;
-        }
-        else if (name.equals(JSEcho)) {
-            sendCallback(callback, data);
-            return true;
-        }
-		return false;
-	}
-
-	@Override
-	protected boolean onUnhandledCallback(String name, JSONObject data) {
-        if (name.equals(JSValuesCallback)) {
-            int result = data.optInt(kResult, 0);
-            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
-            alert.setMessage("result is : "+ result);
-            AlertDialog mAlert = alert.create();
-            mAlert.setCanceledOnTouchOutside(true);
-            mAlert.show();
-            return true;
-        }
-        if (name.equals(JSEchoCallback)) {
-            JSONObject echo = data.optJSONObject(kValues);
-            if (echo != null) {
-                try {
-                    int index = echo.getInt(kIndex);
-                    String stringForTest = echo.getString(kValue);
-                    if (stringForTest.equals(mArrayTest.get(index))) {
-                        Log.d(TAG, "test OK for the String : "+stringForTest);
+    
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        
+        PubSub pubsub = PubSub.getInstance();
+        pubsub.subscribeToChannel(this, JSDoSomeMathsFromWeb);
+        pubsub.subscribeToChannel(this, JSDoSomeMathsFromNativeResponse);
+        pubsub.subscribeToChannel(this, JSAutoTestsFromWeb);
+        pubsub.subscribeToChannel(this, JSAutoTestsFromNativeCallback);
+    }
+    
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+    
+        PubSub pubsub = PubSub.getInstance();
+        pubsub.unsubscribeFromChannel(this, JSDoSomeMathsFromWeb);
+        pubsub.unsubscribeFromChannel(this, JSDoSomeMathsFromNativeResponse);
+        pubsub.unsubscribeFromChannel(this, JSAutoTestsFromWeb);
+        pubsub.unsubscribeFromChannel(this, JSAutoTestsFromNativeCallback);
+    }
+    
+    @Override
+    public void onMessageReceived(@Nullable JSONObject message, @NonNull String channel)
+    {
+        switch(channel)
+        {
+            case JSDoSomeMathsFromNativeResponse:
+                try
+                {
+                    if (message != null)
+                    {
+                        final double result = message.getDouble(kJSResult);
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run()
+                            {
+                                AlertDialog alert = new AlertDialog.Builder(getActivity())
+                                        .setTitle("Result")
+                                        .setMessage("result is : "+ result)
+                                        .setPositiveButton("OK", null)
+                                        .create();
+                                alert.setCanceledOnTouchOutside(true);
+                                alert.show();
+                            }
+                        });
                     }
-                    else {
-                        testFailed = true;
-                        Log.e(TAG, "test failed !!!! send is : "+ mArrayTest.get(index));
-                        Log.e(TAG, "received : "+stringForTest);
-                    }
-                    if (index < mArrayTest.size()-1) {
-                        launchTest(++index);
-                    }
-                    else if (index==mArrayTest.size()-1) {
-                        if (testFailed) {
-                            Toast.makeText(mContext, "Some tests failed ! Check logs.", Toast.LENGTH_SHORT).show();
-                        }
-                        else {
-                            Toast.makeText(mContext, "All tests passed ! No errors", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                } catch (JSONException e) {
+                }
+                catch (JSONException e)
+                {
                     e.printStackTrace();
                 }
-            }
-            return true;
+                break;
+            case JSDoSomeMathsFromWeb:
+                try
+                {
+                    if (message != null)
+                    {
+                        JSONArray values = message.getJSONArray(kJSValues);
+                        double value1 = values.getDouble(0);
+                        double value2 = values.getDouble(1);
+                        String callback = message.getString(Cobalt.kJSCallback);
+    
+                        JSONObject doSomeMathsMessage = new JSONObject();
+                        doSomeMathsMessage.put(kJSResult, value1 + value2);
+    
+                        PubSub.getInstance().publishMessage(doSomeMathsMessage, callback);
+                    }
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+                break;
+            case JSAutoTestsFromWeb:
+                try
+                {
+                    if (message != null)
+                    {
+                        String callback = message.getString(Cobalt.kJSCallback);
+                        
+                        PubSub.getInstance().publishMessage(message, callback);
+                    }
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+                break;
+            case JSAutoTestsFromNativeCallback:
+                try
+                {
+                    if (message != null)
+                    {
+                        int index = message.getInt(kJSIndex);
+                        String value = message.getString(Cobalt.kJSValue);
+                        
+                        String valueSent = mArrayTest.get(index);
+                        
+                        if (value.equals(valueSent))
+                        {
+                            Log.d(TAG, "test OK for the String : " + value);
+                        }
+                        else
+                        {
+                            testFailed = true;
+                            Log.e(TAG, "test failed !!!! send is : " + valueSent);
+                            Log.e(TAG, "received : " + value);
+                        }
+                        
+                        if (index < mArrayTest.size() - 1)
+                        {
+                            launchTest(++index);
+                        }
+                        else if (index == mArrayTest.size() - 1)
+                        {
+                            if (testFailed)
+                            {
+                                Toast.makeText(mContext,
+                                               "Some tests failed ! Check logs.",
+                                               Toast.LENGTH_SHORT)
+                                     .show();
+                            }
+                            else
+                            {
+                                Toast.makeText(mContext,
+                                               "All tests passed ! No errors",
+                                               Toast.LENGTH_SHORT)
+                                     .show();
+                            }
+                        }
+                    }
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+                break;
         }
+    }
 
-		return false;
-	}
+    private void launchTest(final int index)
+    {
+        try
+        {
+            JSONObject message = new JSONObject();
+            message.put(kJSIndex, index);
+            message.put(Cobalt.kJSValue, mArrayTest.get(index));
+            message.put(Cobalt.kJSCallback, JSAutoTestsFromNativeCallback);
 
-	@Override
-	protected void onPullToRefreshRefreshed() { }
-
-	@Override
-	protected void onInfiniteScrollRefreshed() { }
-
-    private void launchTest(int index){
-        JSONObject data = new JSONObject();
-
-            try {
-                JSONObject stringToTest = new JSONObject();
-                stringToTest.put(kIndex, index);
-                stringToTest.put(kValue, mArrayTest.get(index));
-                data.put(kValues, stringToTest);
-
-                sendEvent(JSEcho, data, JSEchoCallback);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+            PubSub.getInstance().publishMessage(message, JSAutoTestsFromNative);
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
