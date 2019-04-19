540466ee8b419a8cf758f2167ca05b568af8ffeb
Sébastien Vitard <contact@sebastienvitard.name>
2019-04-19 16:31:55 +0200

    Update plugin handling

--- Catalog/src/main/java/org/cobaltians/catalog/fragments/PluginsFragment.java
+++ Catalog/src/main/java/org/cobaltians/catalog/fragments/PluginsFragment.java
@@ -4,7 +4,6 @@ import io.kristal.geolocpicker.GeolocPicker;
 import io.kristal.signature.Signature;
 
 import org.cobaltians.cobalt.fragments.CobaltFragment;
-import org.cobaltians.cobalt.plugin.CobaltPluginWebContainer;
 
 import android.content.Intent;
 
@@ -14,14 +13,10 @@ public class PluginsFragment extends CobaltFragment
 	public void onActivityResult(int requestCode, int resultCode, Intent data)
 	{
 		// Forward result to GeolocPicker
-		GeolocPicker geolocPicker = (GeolocPicker) GeolocPicker.getInstance(new CobaltPluginWebContainer(getActivity(),
-																										 this));
-		geolocPicker.onActivityResult(requestCode, resultCode, data);
+		GeolocPicker.getInstance().onActivityResult(requestCode, resultCode, data);
 
 		// Result for Signature
-		Signature signature = (Signature) Signature.getInstance(new CobaltPluginWebContainer(getActivity(),
-																							 this));
-		signature.onActivityResult(requestCode, resultCode, data);
+		Signature.getInstance().onActivityResult(requestCode, resultCode, data);
 
 		super.onActivityResult(requestCode, resultCode, data);
 	}
