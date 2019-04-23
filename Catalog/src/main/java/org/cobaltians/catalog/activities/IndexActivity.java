f8f1dce21fb720508c912364d27b995f67019e57
Sébastien Vitard <contact@sebastienvitard.name>
2019-04-23 18:13:24 +0200

    Update Cobalt

--- Catalog/src/main/java/org/cobaltians/catalog/activities/IndexActivity.java
+++ Catalog/src/main/java/org/cobaltians/catalog/activities/IndexActivity.java
@@ -1,22 +1,9 @@
 package org.cobaltians.catalog.activities;
 
-import org.cobaltians.cobalt.Cobalt;
-import org.cobaltians.cobalt.fragments.CobaltFragment;
-
 public final class IndexActivity extends DefaultActivity
 {
 	@Override
-    protected CobaltFragment getFragment()
-	{
-		return Cobalt.getInstance(this)
-					 .getFragmentForController(CobaltFragment.class,
-											   "index",
-											   "index.html");
-	}
-
-	@Override
-	public String getInitPage()
-	{
-		return "index.html";
+	public String getController() {
+		return "index";
 	}
 }
